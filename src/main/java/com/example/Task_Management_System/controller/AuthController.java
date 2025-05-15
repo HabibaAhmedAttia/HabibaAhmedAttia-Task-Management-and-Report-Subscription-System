ca61f5eca92ed498609924dd9c4b3ff2e9bf32fb
habiba <habibaahmedattia5@gmail.com>
2025-05-15 13:09:09 +0300

    document Authentication APIs using Swagger

--- src/main/java/com/example/Task_Management_System/controller/AuthController.java
+++ src/main/java/com/example/Task_Management_System/controller/AuthController.java
@@ -3,6 +3,7 @@ import com.example.Task_Management_System.dto.AuthResponse;
 import com.example.Task_Management_System.dto.SignInRequest;
 import com.example.Task_Management_System.dto.SignUpRequest;
 import com.example.Task_Management_System.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,21 +11,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
 
+    @Operation(
+            summary = "User sign-up",
+            description = "Register a new user account with full name, email, and password. Returns the saved user object."
+    )
     @PostMapping("/signup")
     public ResponseEntity<String> signUp(@Valid @RequestBody SignUpRequest request) {
         authService.signUp(request);
         return ResponseEntity.ok("User registered successfully");
     }
+    @Operation(
+            summary = "User sign-in",
+            description = "Authenticate user with email and password, and return a JWT token on success."
+    )
     @PostMapping("/signin")
     public ResponseEntity<AuthResponse> signIn(@Valid @RequestBody SignInRequest request) {
         //System.out.println(">>>> /signin called with email: " + request.getEmail());
         AuthResponse response = authService.signIn(request);
         return ResponseEntity.ok(response);
     }
+
 }
